ab6efb44a99fbc03f63d72554291aed987a2e32f
aliado8 <57EujRkb>
2021-01-09 00:33:14 +0500

    class View

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'admin_email' => 'pyatuninvn@yandex.ru',
-    'shop_mail' => 'Магазин ishop2',
+    'shop_name' => 'Магазин ishop2',
     'pagination' => 3,
     'smtp_login' => '',
     'smtp_password' => '',
