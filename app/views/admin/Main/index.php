6509ab62f3d82a3f3836242417176cf8c4ca34fd
Pyatunin Vitaliy <pv@solar-studio.net>
2021-04-21 11:17:34 +0500

    вывод данных на главной в админке в виджеты

--- app/views/admin/Main/index.php
+++ app/views/admin/Main/index.php
@@ -24,14 +24,14 @@
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3><?=$countNewOrders?></h3>
 
-                        <p>New Orders</p>
+                        <p>Заказов</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-bag"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="<?=ADMIN?>/order" class="small-box-footer">Все заказы <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -39,14 +39,14 @@
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+                        <h3><?=$countProducts?></h3>
 
-                        <p>Bounce Rate</p>
+                        <p>Товаров</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="<?=ADMIN?>/product" class="small-box-footer">Все товары <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -54,14 +54,14 @@
                 <!-- small box -->
                 <div class="small-box bg-warning">
                     <div class="inner">
-                        <h3>44</h3>
+                        <h3><?=$countUsers?></h3>
 
-                        <p>User Registrations</p>
+                        <p>Пользователей</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-person-add"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="<?=ADMIN?>/user" class="small-box-footer">Все пользователи <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
@@ -69,14 +69,14 @@
                 <!-- small box -->
                 <div class="small-box bg-danger">
                     <div class="inner">
-                        <h3>65</h3>
+                        <h3><?=$countCategories?></h3>
 
-                        <p>Unique Visitors</p>
+                        <p>Категорий</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-pie-graph"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    <a href="<?=ADMIN?>/category" class="small-box-footer">Все категории <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
